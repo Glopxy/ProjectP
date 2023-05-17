1e745cf66a29dac4eded5665347a0e8c4bf98320
Furkan <furkankosaci@gmail.com>
2023-05-17 17:26:33 +0300

    test

--- ProjectP/Controllers/InternController.cs
+++ ProjectP/Controllers/InternController.cs
@@ -7,7 +7,7 @@ using PinternAPI.Core.Application.Features.CQRS.Queries;
 
 namespace PinternAPI.Controllers
 {
-    [Authorize(Roles ="Admin")]
+    //[Authorize(Roles ="Admin")]
     [Route("api/[controller]")]
     [ApiController]
     public class InternController : ControllerBase
